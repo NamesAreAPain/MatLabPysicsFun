d668cb25bae6eaa36bb2a8362cd12fbf7eab1e0e
hribalj <jhribal@lion.lmu.edu>
2017-02-19 20:40:12 -0800

    added documentation

--- MultiPoint/distance.m
+++ MultiPoint/distance.m
@@ -1,3 +1,4 @@
+%calculates the scalar distance between points A and B
 function d = distance(PointA, PointB)
     d = (PointA.loca(1) - PointB.loca(1))^2 + (PointA.loca(2) - PointB.loca(2))^2;
     d = sqrt(d);
