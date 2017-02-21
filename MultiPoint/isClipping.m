1afe9fe75e00be002ec613cda1b9bb7132cd1e2a
hribalj <hribalj@users.noreply.github.com>
2017-02-20 21:50:13 -0800

    added documentation

--- MultiPoint/isClipping.m
+++ MultiPoint/isClipping.m
@@ -1,3 +1,4 @@
+%looks at the vetices of the network and calculates the angle between the vertices and the point of the other network to see if it has overlaped
 function bool = isClipping(pntA, vertices)
     dVecs = zeros(length(vertices),2);
     for iter = 1:length(vertices)
