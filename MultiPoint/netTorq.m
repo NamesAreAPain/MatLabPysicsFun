9468baa57475f6a754618c12ffbc51604acaf16f
hribalj <hribalj@users.noreply.github.com>
2017-02-20 22:43:06 -0800

    added documentation

--- MultiPoint/netTorq.m
+++ MultiPoint/netTorq.m
@@ -1,8 +1,9 @@
+%function calculates the net torque of the network
 function netTorq = netTorq(network)
-   netTorq = 0;
-   com = centerOfMass(network);
-   for P = network.pnts
-       cp = cross([disVec(com,P) 0],[netForce(P) 0]);
+   netTorq = 0; %initially sets net torque to 0
+   com = centerOfMass(network); %calculates the center of mass of the network
+   for P = network.pnts %takes a set of points that make up the network
+       cp = cross([disVec(com,P) 0],[netForce(P) 0]); %takes the cross product of the distance between the center of mass and a point and the netForce 
        netTorq = netTorq + cp(3);
    end
 end
