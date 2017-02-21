662f8d927f792b031c0c5b0ee5d590269c0b0ea8
hribalj <hribalj@users.noreply.github.com>
2017-02-20 22:12:27 -0800

    added documentation

--- MultiPoint/netForce.m
+++ MultiPoint/netForce.m
@@ -1,4 +1,5 @@
+%function calculates the net force 
 function [Fx , Fy ] = netForce(point)
-    Fx = sum(point.forc (:,1));
-    Fy = sum(point.forc (:,2));
+    Fx = sum(point.forc (:,1)); %calculates the net force in the x direction
+    Fy = sum(point.forc (:,2)); %calculates the net force in the y direction 
 end
