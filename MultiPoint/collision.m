8d0d5644ed8f9a02a48c2135ad90d79f9ee07ab7
NamesAreAPain <jbcrocker@pacbell.net>
2017-02-26 15:07:54 -0800

    Fixed -1 bug

--- MultiPoint/collision.m
+++ MultiPoint/collision.m
@@ -2,7 +2,7 @@ function Network = collision(Network,Networks)
     for n = 1:length(Networks)
        if(isColliding(Network,Networks(n)))
            norm = disVec(struct('loca',centerOfSpace(Networks(n))),struct('loca',centerOfSpace(Network))); 
-           impl = -1*(2)*dot((Networks(n).velo - Network.velo),norm)/(1/Network.mass + 1/Networks(n).mass);
+           impl = (2)*dot((Networks(n).velo - Network.velo),norm)/(1/Network.mass + 1/Networks(n).mass);
            Network = applyImpulse(Network,impl * norm);
        end
     end
