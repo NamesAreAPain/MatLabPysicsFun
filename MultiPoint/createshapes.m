376d7b39acdc33d48972b1972200aef7503f636d
hribalj <hribalj@users.noreply.github.com>
2017-03-29 21:55:31 -0700

    Restored to before we talked

--- MultiPoint/createshapes.m
+++ MultiPoint/createshapes.m
@@ -1,8 +1,9 @@
 function creatshapes(Networks)
-
+global axisplot;
+axisplot;
 for N = 1:length(Networks)
-    X = zeros(1,length(Networks(N).pnts);
-    Y = zeros(1,length(Networks(N).pnts);
+    X = zeros(1,length(Networks(N).pnts));
+    Y = zeros(1,length(Networks(N).pnts));
     for P = 1:length(Networks(N).pnts)
         X(P) = Networks(N).pnts(P).loca(1);
         Y(P) = Networks(N).pnts(P).loca(2); 
