6eba0efe40c78fd0bfb345dc690d606b6b73b46d
hribalj <hribalj@users.noreply.github.com>
2017-04-02 21:01:13 -0700

    patch stuff

--- MultiPoint/createshapes.m
+++ MultiPoint/createshapes.m
@@ -1,12 +1,13 @@
-function creatshapes(Networks)
-global axisplot;
-axisplot;
+function creatshapes(Networks,axisJ)
+axisJ;
+cla;
+% object = zeros(1,length(Networks));
 for N = 1:length(Networks)
     X = zeros(1,length(Networks(N).pnts));
     Y = zeros(1,length(Networks(N).pnts));
     for P = 1:length(Networks(N).pnts)
         X(P) = Networks(N).pnts(P).loca(1);
-        Y(P) = Networks(N).pnts(P).loca(2); 
+        Y(P) = Networks(N).pnts(P).loca(2);
     end
-   patch(X,Y,'k');
+    patch(axisJ,X,Y,'k');
 end
