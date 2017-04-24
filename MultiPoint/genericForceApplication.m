225bdeef6501769b2af2ef0b5ca6f377a74b2487
NamesAreAPain <jbcrocker@pacbell.net>
2017-04-24 00:46:09 -0700

    Diagram

--- MultiPoint/genericForceApplication.m
+++ MultiPoint/genericForceApplication.m
@@ -1,8 +1,8 @@
 function Network = genericForceApplication(Network,Networks,func)
     for psub = 1:length(Network.pnts)
-        for n = Networks
-            for pobj = n.pnts
-                Network.pnts(psub).forc = [Network.pnts(psub).forc; func(Network.pnts(psub),n.pobj)];
+        for n = 1:length(Networks)
+            for pobj = 1:length(n.pnts)
+                Network.pnts(psub).forc = [Network.pnts(psub).forc; func(Network.pnts(psub),Networks(n).pnts(pobj))];
             end
         end
     end
