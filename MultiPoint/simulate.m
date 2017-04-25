3c1192cd576b1ba9c72cd1a8dcbf4a6369799b35
hribalj <hribalj@users.noreply.github.com>
2017-04-24 23:58:40 -0700

    documentation added

--- MultiPoint/simulate.m
+++ MultiPoint/simulate.m
@@ -1,7 +1,7 @@
-%function takes inputs and runs the simulation
+%this is the main function that allows the simulation to run 
 function Networks = simulate(Networks,pntForceFuncs,netwForceFuncs,interval,time,options,axisJ)
-global ENDING;
-global TICKTIME
+global ENDING; % sets global variable for the pause button called ENDING
+global TICKTIME % sets global variable to allow the simulation to be slowed down and sped up
 eTime = 0;
 disp('simulation start');
 while eTime < time
@@ -17,11 +17,11 @@ while eTime < time
         for F = 1:length(netwForceFuncs)
             Networks(N) = netwForceFuncs{F}(Networks(N));
         end
-        Networks(N) = moveNet(Networks(N),interval);
-        Networks(N) = rotateNet(Networks(N),interval);
+        Networks(N) = moveNet(Networks(N),interval); %moves the networks
+        Networks(N) = rotateNet(Networks(N),interval); %rotates the networks if torque is being applied
     end
     if length(Networks) ~= 1
-        combos = nchoosek(1:length(Networks),2);
+        combos = nchoosek(1:length(Networks),2); % tests al of the combinations of the vertices to check if a collision is happening
         s = size(combos);
         for y = 1:(s(1))
             Networks = collision(Networks,combos(y,1),combos(y,2));
